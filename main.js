9236348ac7b1503eb7073c1182171d65155ab758
Shivang Ayush <shivangayush161161@gmail.com>
2025-08-09 19:51:21 +0530

    Doing something

--- main.js
+++ main.js
@@ -4,21 +4,29 @@
 })();
 
 document.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById('contact-form');
+  const form = document.getElementById('booking-form');
   const status = document.querySelector('.form-status');
   if(form) {
     form.addEventListener('submit', function(e) {
       e.preventDefault();
       emailjs.sendForm('service_dk8b18h', 'template_skdj8en', this)
         .then(function() {
-          status.textContent = "Thank you for reaching out! We'll get back to you soon.";
+          alert("Booking submitted!");
           form.reset();
         }, function(error) {
-          status.textContent = "Oops! Something went wrong. Please try again.";
+          alert("Oops! Something went wrong. Please try again.");
         });
     });
   }
 });
+
+function showDetails(info) {
+    document.getElementById('details').innerText = info;
+}
+
+function hideDetails() {
+    document.getElementById('details').innerText = '';
+}
 
 // Optional: For the "Get Started" button
 function showAlert() {
